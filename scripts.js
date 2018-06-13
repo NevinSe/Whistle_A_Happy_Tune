1fc94bbfbc6fc8a45629f84249a11156aef3aa03
NevinSe <kalawhar@gmail.com>
2018-06-13 14:37:30 -0500

    JSON working

--- scripts.js
+++ scripts.js
@@ -1,47 +1,52 @@
-/*
-let url = '';
 
-function getWeather() {
-    let url = '';
-
-
-}
-
-
-
-function getURL(){
-    let http = `api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apikey}&units=${units}`;
-    let city = document.getElementById('cityTextbox').value;
-    let apikey = '&appid=1eb1c09b4557ae2856528ee4c3560580';
-    let units = '';
-
-    if(document.getElementById('metric').checked === true){
-        units += '&units=' + document.getElementById('metric').value;
-    } else{
-        units += '&units=' + document.getElementById('imperial').value;
-    }
-
-    url += http;
-}*/
-
-//function that gets user search term
 function getSearchValue(){
     let userSearchTerm = document.getElementById('searchTermTextBox').value.toLowerCase();
     let myArr = userSearchTerm.split(' ');
     let myStr = '';
-    for(i = 0; i < myArr.length; i++){
+    for(let i = 0; i < myArr.length; i++){
         myStr += myArr[i];
         if(i !== myArr.length - 1){
             myStr += '+';
         }
     }
-    return myStr;
+    return `term=${myStr}`;
+}
 
+function getExplicitValue(){
+   if(document.getElementById('explicitYes').checked === true){
+       return document.getElementById('explicitYes').value;
+   } else {
+       return document.getElementById('explicitNo').value;
+   }
+}
 
+function getURLString(userSearchTerm){
+    let country = 'country=us&';
+    let media = 'media=music&';
+    let explicit = getExplicitValue();
+    let language = 'lang=en_us&';
+    let url = `https://itunes.apple.com/search?${country}${media}${language}${explicit}${userSearchTerm}`;
+    console.log(url);
+    return url;
 }
 
 //function ran on search button click
 function searchButton(){
-    let userSearch = getSearchValue();
-    console.log(userSearch);
+    let userSearchTerm = getSearchValue();
+    let url = getURLString(userSearchTerm);
+    let JSON = loadJSON(url);
+}
+
+function loadJSON(url){
+    let xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        console.log("hello");
+        if(this.readyState === 4 && this.status === 200){
+            document.getElementById("whatever").innerHTML = this.responseText;
+            console.log(JSON.parse(this.responseText));
+        }
+
+    };
+    xhttp.open("GET", url, true);
+    xhttp.send();
 }
