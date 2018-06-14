07f7c6004b3f6ead48e159424de7a0ceac6b9c57
EleMint <dylanschmidt0216@gmail.com>
2018-06-14 07:54:17 -0500

    Took out global variables

--- scripts.js
+++ scripts.js
@@ -1,6 +1,4 @@
 
-let myObj = {};
-
 function getSearchValue(){
     let userSearchTerm = document.getElementById('searchTermTextBox').value.toLowerCase();
     let myArr = userSearchTerm.split(' ');
@@ -33,42 +31,37 @@ function getURLString(userSearchTerm){
 
 }
 
-//function ran on search button click
+
 function searchButton(){
     let userSearchTerm = getSearchValue();
     let url = getURLString(userSearchTerm);
     loadJSON(url);
-    let audioObject = document.getElementById('audio');
-    let audioTag = document.getElementById('audioTag');
-    //console.log(JSON);
-    console.log('this is my object:', myObj);
-    console.log('first song url', myObj.resultCount);
-    //audioObject.src = `${myObj.results[0].previewUrl}`;
-    //audioTag.src = `${myObj.results[0].previewUrl}`;
 }
-/*document.addEventListener('keypress', function(e){
+document.addEventListener('keypress', function(e){
     let key = e.which || e.keyCode;
     if (key === 13) {
         let userSearchTerm = getSearchValue();
         let url = getURLString(userSearchTerm);
-        let JSON = loadJSON(url);
-        console.log(JSON);
-        console.log('this is my object:', myObj);
-        console.log('audio link', myObj.results[0].previewUrl);
-        document.getElementById('audio')[1].setAttribute('src', myObj.results[0].previewUrl);
+        loadJSON(url);
     }
-});*/
+});
+function secondMaster(myObj ,songPreviewUrl){
+    let audioObject = document.getElementById('audio');
+    let audioTag = document.getElementById('audioTag');
+    audioObject.src = songPreviewUrl;
+    audioTag.src = songPreviewUrl;
+    document.getElementById('displayOneSong').innerHTML = 'Hello';
+}
 
 function loadJSON(url){
     let xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
         if(this.readyState === 4 && this.status === 200) {
             let bacon = JSON.parse(this.responseText);
-            document.getElementById("display").innerHTML = this.responseText;
-
-            myObj[0] = bacon;
-
-
+            console.log(bacon);
+            document.getElementById("display").innerHTML= bacon;
+            let songPreviewUrl = myObj.results[0].previewUrl;
+            secondMaster(bacon ,songPreviewUrl);
         }
     };
     xhttp.open("GET", url, true);
